629308a1ba68e6fb559312d7377fafd9d62389c5
Fernando Chorney <gitlab@djsbx.com>
2018-03-26 11:11:02 -0500

    Basic bash setup for raspberry pi3

---
+++ .pythonrc.py
@@ -0,0 +1,19 @@
+import os
+
+try:
+    import readline
+except ImportError:
+    pass
+else:
+    import rlcompleter
+    readline.parse_and_bind("tab: complete")
+
+    histfile = os.path.join(os.path.expanduser("~"), ".pyhist")
+    try:
+        readline.read_history_file(histfile)
+        readline.set_history_length(1000)
+    except IOError:
+        pass
+
+    import atexit
+    atexit.register(readline.write_history_file, histfile)
